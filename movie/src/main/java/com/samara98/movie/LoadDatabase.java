759a7b81fa164d371fc5916a6c763dba8f0ffa7e
Sulthon Abdul Malik <sulthon.abdmalik@gmail.com>
2021-09-14 08:19:32 +0700

    Movie Database

---
+++ movie/src/main/java/com/samara98/movie/LoadDatabase.java
@@ -0,0 +1,20 @@
+package com.samara98.movie;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoadDatabase {
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(MovieRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Movie("Hero", "A defense officer, Nameless, was summoned by the King of Qin regarding his success of terminating three warriors.", "https://m.media-amazon.com/images/M/MV5BMWQ2MjQ0OTctMWE1OC00NjZjLTk3ZDAtNTk3NTZiYWMxYTlmXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_QL75_UX190_CR0,0,190,281_.jpg", 7.9)));
+            log.info("Preloading " + repository.save(new Movie("Avengers: Endgame", "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.", "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_QL75_UX190_CR0,0,190,281_.jpg", 8.4)));
+        };
+    }
+}
